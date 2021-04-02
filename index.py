a81790f13ae9e5cd861398cb3f87db7abc07fef2
ArthurOliveiraTeles <arthur8teles@gmail.com>
2021-04-02 20:16:59 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from funcoes1 import roleta10
 
 
 app = Flask("Youtube")
-
+CORS(app)
 
 @app.route("/gacha", methods=["GET"])
 def gachaSystem():
